c83c826de5da86d5d4157019c878ba7e7e7c80be
Zenish <zenish77@gmail.com>
2021-05-25 10:51:54 +0545

    Difference between / and *

--- FunctionIntrospection.py
+++ FunctionIntrospection.py
@@ -25,3 +25,37 @@ func is bound to my_obj, an instance of MyClass
 
 isroutine(my_func) → True
 isroutine(my_obj.func) → True'''
+
+
+
+
+def myFunc(x,y,/,kw):
+    """/ indicates that before this only postional arguments can exist no keyword arguments"""
+    print(x)
+    print(y)
+    print(kw)
+
+
+print(help(myFunc))
+myFunc.category="TestBenchmark"
+
+print(myFunc.category)
+
+print(dir(myFunc))
+
+
+myFunc(1,2,kw=4)
+
+
+
+
+
+
+def myFunc(x,y,*,kw):
+    '''* indicates that after star no postional arguments can exist only keword only arguments'''
+    print(x)
+    print(y)
+    print(kw)
+
+myFunc(1,2,kw=114)
+
--- swap.py
+++ swap.py
@@ -1,7 +1,22 @@
-x=[2,3]
-for i in x:
-    print(i)
-# swap(x[0],x[1])- this doesnt work
-x[0], x[1] = x[1], x[0]
-for i in x:
-    print("after swap",i)
+# x=[2,3]
+# for i in x:
+#     print(i)
+# # swap(x[0],x[1])- this doesnt work
+# x[0], x[1] = x[1], x[0]
+# for i in x:
+#     print("after swap",i)
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+    
+    while True:
+
+        notification.notify(
+            title="Drink Water Notification!",
+            message="Chacha Paani Pilo!!! Marjaoge. Chacha Rest Karlijiye Warna Rest In Peace Hojayenge",
+            app_icon="/Project 2/water-bottle.png",
+            timeout=12
+        )
+
+        time.sleep(60*60)
