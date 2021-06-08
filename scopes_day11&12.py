60058782a50143418afee2825237601eaeb6c05c
Zenish <zenish77@gmail.com>
2021-06-08 21:11:28 +0545

    closure with instance

--- scopes_day11&12.py
+++ scopes_day11&12.py
@@ -1,7 +1,7 @@
 'Global scopes lies within built-in scopes and each of the modules store em in their own namespace'
 'builtins have there own namespace and modules have their own namespace'
 
-#for example 
+# for example
 # print(a) #this throws an error because it looks for print and a on this module's namespace
 # but doesn't find any then it searches on the built-in namespace where print is found but not a hence the error
 
@@ -18,45 +18,52 @@
 'The above error occurs because while compiling func() a is assigned a value so it is now local but the print statement occurs before the assignment it does not know what it is'
 
 
-
-
-
-#NonLocal scopes are found in between nested functions
+# NonLocal scopes are found in between nested functions
 
 def outer_func():
-    x="Hello"
+    x = "Hello"
 
     def inner_func():
-        x="World"
+        x = "World"
 
     inner_func()
     print(x)
 
+
 outer_func()
 
-#theoutput is Hello because the x inside inner function has a local scope inside it
+# theoutput is Hello because the x inside inner function has a local scope inside it
+
+
 def outer_func():
-    x="Hello"
+    x = "Hello"
 
     def inner_func():
         nonlocal x
-        x="World"
+        x = "World"
 
     inner_func()
     print(x)
 
+
 outer_func()
 
 
+# the output is now World because the nonlocal scope of x inside inner_funct was termed as nonlocal
+'''Enclosing (or nonlocal) scope is a special scope that only exists for nested functions. If the local scope is an inner or nested function, then the enclosing scope is the scope of the outer or enclosing function. This scope contains the names that you define in the enclosing function. The names in the enclosing scope are visible from the code of the inner and enclosing functions.
 
-#the output is now World because the nonlocal scope of x inside inner_funct was termed as nonlocal
+'''
 
 x = 100
+
+
 def outer():
     x = 'python'  # masks global x
+
     def inner1():
         nonlocal x  # refers to x in outer
-        x = 'monty' # changed x in outer scope
+        x = 'monty'  # changed x in outer scope
+
         def inner2():
             global x  # refers to x in global scope
             x = 'hello'
@@ -65,5 +72,7 @@ def outer():
         print('inner1 (after):', x)
     inner1()
     print('outer', x)
+
+
 outer()
 print(x)
