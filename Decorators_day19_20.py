d58de7d5c1e8a631f879bae72556f41806df03e7
Zenish <zenish77@gmail.com>
2021-06-16 17:12:45 +0545

    decorators using reduce

--- Decorators_day19_20.py
+++ Decorators_day19_20.py
@@ -30,6 +30,7 @@ my_func = func(my_func)
 
 
 from functools import wraps
+from typing import SupportsAbs
 def counter(fn):
     count = 0
 
@@ -111,3 +112,84 @@ print(help(mult))
 mult = counter(mult)
 print(help(mult))
 print(mult(1, 2, 4, d=9))
+
+
+# Day20
+
+
+def timed(fn):
+    from time import perf_counter
+    from functools import wraps
+
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        start = perf_counter()
+        result = fn(*args, **kwargs)
+        end = perf_counter()
+        elapsed = end - start
+
+        args_ = [str(a) for a in args]
+        kwargs_ = ['{0}={1}'.format(k, v) for (k, v) in kwargs.items()]
+        all_args = args_ + kwargs_
+        args_str = ','.join(all_args)
+        print('{0}({1}) took {2:.6f}s to run.'.format(fn.__name__,
+                                                      args_str,
+                                                      elapsed))
+        return result
+
+    return inner
+
+
+@timed
+def calc_recursive_fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return calc_recursive_fib(n-1) + calc_recursive_fib(n-2)
+
+
+print(calc_recursive_fib(9))
+
+
+@timed
+def fib_recursed(n):
+    return calc_recursive_fib(n)
+
+
+print(fib_recursed(10))
+
+'''Using Reduce
+We first need to understand how we are going to calculate the Fibonnaci sequence using reduce:
+
+n=1:
+(1, 0) --> (1, 1)
+
+n=2:
+(1, 0) --> (1, 1) --> (1 + 1, 1) = (2, 1)  : result = 2 
+
+n=3
+(1, 0) --> (1, 1) --> (2, 1) --> (2+1, 2) = (3, 2)  : result = 3
+
+n=4
+(1, 0) --> (1, 1) --> (2, 1) --> (3, 2) --> (5, 3)  : result = 5
+In general each step in the reduction is as follows:
+
+previous value = (a, b)
+new value = (a+b, a)
+If we start our reduction with an initial value of (1, 0), we need to run our "loop" n times.
+
+We therefore use a "dummy" sequence of length n to create n steps in our reduce.'''
+
+
+@timed
+def fib_reduce(n):
+    from functools import reduce
+    initial = (1, 0)
+    dummy = range(n-1)
+    fib_n = reduce(lambda prev, n: (prev[0] + prev[1], prev[0]),
+                   dummy,
+                   initial)
+    return fib_n[0]
+
+
+print(fib_reduce(40))
--- reducingFunctions_Day6.py
+++ reducingFunctions_Day6.py
@@ -1,42 +1,44 @@
 
+import functools
 import math
 print("______________________________________________Day 6___________________________________________________________________")
 
-mapvalue=map(math.factorial,range(10))
+mapvalue = map(math.factorial, range(10))
 for x in mapvalue:
-    print(x)      #because map returns iterator not list so we need to convert it into a list to print it beforehand
-
-
+    print(x)  # because map returns iterator not list so we need to convert it into a list to print it beforehand
 
 
 'Reducing function is also called as aggrefators and folding functions'
 
-l=[5,6,3,56,100]
-max=l[0]
+l = [5, 6, 3, 56, 100]
+max = l[0]
 for x in l:
-    if max<x:
-        max=x
+    if max < x:
+        max = x
 
 print(max)
 print("The result of the above function in reduced format is as follow\n")
-def reducedFunc(fn,l):
-    result=l[0]
+
+
+def reducedFunc(fn, l):
+    result = l[0]
     for x in l[1:]:
-        result=fn(result,x)
+        result = fn(result, x)
     print(f"{result} is the maximum among the list {l}")
 
-reducedFunc(lambda x,y:x if x>y else y,l)
+
+reducedFunc(lambda x, y: x if x > y else y, l)
 
 'Python has its own builtin reduce functions'
-import functools
 
-l=[0,'',None,100]
+l = [0, '', None, 100]
 
-print(functools.reduce(lambda x,y:bool(x) or bool(y),l))
+print(functools.reduce(lambda x, y: bool(x) or bool(y), l))
 'Reduce needs to be imported from functools but not "any"'
 print(f"THe value is checked using any {any(l)}")
 
-l=[1,2,3,4]
+l = [1, 2, 3, 4]
 
 'Reduce also has an initializer'
-print(functools.reduce(lambda x,y:x+y,l,100))  #here initializer is 100 , its equivalent to initializing l[0]=100 then the remaining l follows
+# here initializer is 100 , its equivalent to initializing l[0]=100 then the remaining l follows
+print(functools.reduce(lambda x, y: x+y, l, 100))
