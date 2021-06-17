f207fbbcbacfaa99ed61d53016e21e4bce36c5df
Zenish <zenish77@gmail.com>
2021-06-17 21:41:54 +0545

    HackerRank counting string Memeory error

---
+++ countingstring.py
@@ -0,0 +1,31 @@
+# HackerRank Question
+
+n = 5
+x = 'asscsc'
+count = 0
+infine = x*(n+1)
+print(f"this is the actual string {infine}")
+toStringg = [separated for separated in infine]
+print(toStringg)
+for x, y in enumerate(toStringg):
+    if(x < n and y == "a"):
+        count = count+1
+print(count)
+# isd = ["K", 'a', 'j', 'o', 'l']
+# print(infine)
+# for x, y in enumerate(isd):
+#     print(f"X is {x} and Count is {y}")
+# # limit = [z for z, y in enumerate(infine) if y <= n]
+# # print(limit)
+# for(i=0;i<10;i++)
+print("__________________________________")
+n = 1000000000000
+x = 'a'
+count = 0
+infine = x*(n+1)
+print(f"this is the actual string {infine}")
+# print(infine[1])
+# toStringg = [separated for separated in infine]
+for x in range(n):
+    alpha = alpha+infine[x]
+print(alpha.count('a'))
